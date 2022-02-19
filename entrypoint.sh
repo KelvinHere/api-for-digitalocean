31f3fef49b4349967559d1eae686356a34dbe3bf
KelvinHere <kel@DESKTOP-N5MBM51.localdomain>
2022-02-19 18:52:56 +0000

    BUGFIX - Error wrong directory in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 python manage.py migrate --no-input
-python restexample/manage.py loaddata dictionary.json
+python manage.py loaddata dictionary.json
 python manage.py collectstatic --no-input
 
 gunicorn api.wsgi:application --bind 0.0.0.0:8000
