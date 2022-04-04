5fdeebe2b033a949474c8b3733b25e5a7d406540
KelvinHere <kel@DESKTOP-N5MBM51.localdomain>
2022-04-04 18:44:25 +0100

    FEAT: Add token auth

--- myapp/api/settings.py
+++ myapp/api/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'word',
 ]
 
@@ -150,5 +151,6 @@ REST_FRAMEWORK = {
         'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ]
     }
--- myapp/word/urls.py
+++ myapp/word/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import RandomWord, home, WordDetail
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('', home, name='home'),
     path('word/', RandomWord.as_view(), name='word'),
-    path('word/<int:id>/', WordDetail.as_view())
+    path('word/<int:id>/', WordDetail.as_view()),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
